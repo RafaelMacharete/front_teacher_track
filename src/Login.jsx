8bd18233b5c2a5fa1aed04c22de40c82111ff1fa
Macharete_r <rafaelmacharete00@gmail.com>
2025-03-04 00:02:59 -0300

    testing: useState

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,40 @@
-import './Login.css'
+import React, { useState } from 'react';
+import './Login.css';
 
-function App(props) {
+function App() {
+  const [modal, setModal] = useState(false);
+
+  function toggleModal() {
+    setModal(!modal);
+  }
 
   return (
     <div>
-      <form action="GET">
-      <img src="src/assets/images/senai_logo.png" alt="senai logo" width='234px' height='60px'/>
-        <label htmlFor="nif">NIF</label>
-        <input type="text" name="nif" />
-        <label htmlFor="senha">Senha</label>
-        <input type="password" name='senha' />
-        <button type="submit">Entrar</button>
-      </form>
+      <button onClick={toggleModal}>Open</button>
+
+      {modal && (
+        <>
+          <form action="GET">
+            <img src="src/assets/images/senai_logo.png" alt="senai logo" width="234px" height="60px" />
+            <label htmlFor="nif">NIF</label>
+            <input type="text" name="nif" />
+            <label htmlFor="senha">Senha</label>
+            <input type="password" name="senha" />
+            <button type="submit">Entrar</button>
+          </form>
+
+          <div className="modal">
+            <div className="overlay" onClick={toggleModal}>asdas</div>
+            <div className="modal-content">
+              <h2>Hello Modal!</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <button className="close-modal" onClick={toggleModal}>Close</button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
