4801ff6714b41621ba57b66f2d21489ef0ec3215
Macharete_r <rafaelmacharete00@gmail.com>
2025-03-05 16:41:18 -0300

    schedule table

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,7 +1,14 @@
 import "./Profile.css";
+import React, { useState } from 'react';
 import Navbar from "./components/Navbar.jsx";
 
 function Profile() {
+  const [dropdown, setDropdown] = useState(false);
+
+  function toggleDropdown() {
+    setDropdown(!dropdown);
+  }
+
   return (
     <>
       <Navbar />
@@ -15,15 +22,85 @@ function Profile() {
             <li>NIF: 010203</li>
           </ul>
         </div>
-        <div>
+        {/* Teacher Schedule */}
+        <div className="teacher-schedule">
           <h1>Ocupação Docente</h1>
-          <div>
-            <ul>
-              <li>Manhã</li>
-              <li>Tarde</li>
-              <li>Noite</li>
-            </ul>
+          {/* Dropdown */}
+          <div className="shifts-container">
+            <div className="menu"
+            >
+              <button
+                onClick={toggleDropdown}
+                className="menu-button"
+              // onBlur={() => setTimeout(() => setDropdown(false))} 
+              >
+                <span>Manhã</span>
+                <img src="./public/caret-down-fill.svg" alt="arrow pointing down (menu)" />
+              </button>
+
+              {dropdown && (
+                <nav className="dropdown-content">
+                  <ul>
+                    <li>Tarde</li>
+                    <li>Noite</li>
+                  </ul>
+                </nav>
+              )}
+            </div>
           </div>
+
+          <table border="10">
+            <tr>
+              <th>Horário</th>
+              <th>Segunda</th>
+              <th>Terça</th>
+              <th>Quarta</th>
+              <th>Quinta</th>
+              <th>Sexta</th>
+            </tr>
+            <tr>
+              <td class="highlight">07:00</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td class="highlight">08:00</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td class="highlight">09:00</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td class="highlight">10:00</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td class="highlight">11:00</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+
+
         </div>
       </div>
     </>
