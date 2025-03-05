43b335e0b3a63b812f320e6a33106922dc71fcbd
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-05 11:25:06 -0300

    styling: Profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,37 +1,33 @@
-import './Profile.css'
-import Header from './components/Header.jsx'
+import "./Profile.css";
+import Header from "./components/Header.jsx";
 
 function Profile() {
-    return (
-        <>
-            <Header/>
-            <div>
-                <img src="" alt="" />
-                <ul>
-                    <li>Professor da Silva</li>
-                    <li>professor.silva@senaisp.edu.br</li>
-                    <li>(19) 9 8765-4321</li>
-                    <li>NIF: 010203</li>
-                </ul>
-            </div>
-            <div>
-                <h1>Ocupação Docente</h1>
-                <div>
-                    <ul>
-                        <li>
-                            Manhã
-                        </li>
-                        <li>
-                            Tarde
-                        </li>
-                        <li>
-                            Noite
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    )
-};
+  return (
+    <>
+      <Header />
+      <div className="content-profile">
+        <div className="profile-info">
+          <img src="./public/person-circle.svg" alt="" />
+          <ul>
+            <li>Professor da Silva</li>
+            <li>professor.silva@senaisp.edu.br</li>
+            <li>(19) 9 8765-4321</li>
+            <li>NIF: 010203</li>
+          </ul>
+        </div>
+        <div>
+          <h1>Ocupação Docente</h1>
+          <div>
+            <ul>
+              <li>Manhã</li>
+              <li>Tarde</li>
+              <li>Noite</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Profile;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="navbar">
       <ul>
-        <img src="./images/senai_logo.png" alt="logo senai" className="image" />
+        <img src="./images/senai_logo.png" alt="logo senai" className="image" height="45px"/>
         <div>
           <li>Início</li>
           <li>Cadastros</li>
