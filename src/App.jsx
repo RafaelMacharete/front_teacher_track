cf025f0ee2c8621ad8acd02dc07b87c87067ed7d
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-10 15:33:52 -0300

    connecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,41 @@
-import react from "react"
-import Login from "./pages/Login"
+import react from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Home from "./pages/Home.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Profile from "./pages/Profile.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
+function Logout(){
+    localStorage.clear();
+    return <Navigate to='/login' />
+}
+
+function RegisterAndLogout(){
+    localStorage.clear()
+    return <Register />
+}
+
+function App(){
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                >
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<RegisterAndLogout />} />
+                <Route path="*" element={<NotFound /> }></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from "./App.jsx"
 import './index.css';
-import Login from './pages/Login.jsx';
-import Profile from './pages/Profile.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={ <Login />} />
-        <Route path="/profile" element={ <Profile />} />
-      </Routes>
-    </Router>
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
